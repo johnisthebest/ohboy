438f7cdf2cd59ad42da649d36c7f9b47a595279a
johnisthebest <48302154+johnisthebest@users.noreply.github.com>
2019-04-21 19:49:58 -0400

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,32 @@
+//middle
+
+function positionTheDot() {
+
+  // What percentage down the page are we? 
+  var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+  // Get path length
+  var path = document.getElementById("theMotionPath3");
+ 
+  var pathLen = path.getTotalLength();
+ 
+  
+  // Get the position of a point at <scrollPercentage> along the path.
+  var pt = path.getPointAtLength(scrollPercentage * pathLen);
+  
+  
+  // Position the red dot at this point
+  var dot = document.getElementById("dot3");
+ 
+  dot.setAttribute("transform", "translate("+ pt.x + "," + pt.y + ")");
+  
+  
+};
+
+// Update dot position when we get a scroll event.
+window.addEventListener("scroll", positionTheDot);
+
+// Set the initial position of the dot.
+positionTheDot();
+
+
